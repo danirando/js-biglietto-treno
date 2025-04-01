64d7b73d720bcb5ad0855628fb3d6cdaffdd38f6
danirando <danielerando@tiscali.it>
2025-04-01 14:57:15 +0200

    add isnan

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
-const kmDaPercorrere = parseInt(prompt("Quanti km vuoi percorrere?"));
+const kmDaPercorrere = prompt("Quanti km vuoi percorrere?");
+
+if (isNaN(kmDaPercorrere)) {
+  alert("Inserire un numero per indicare i Km da percorrere");
+}
 
 console.log(`"Km da percorrere: ${kmDaPercorrere}`);
 
-const anni = parseInt(prompt("Quanti anni hai?"));
+const anni = prompt("Quanti anni hai?");
+
+if (isNaN(anni)) {
+  alert("Inserire un numero per indicare gli anni");
+}
 
 console.log(`"Anni: " ${anni}`);
 
@@ -20,6 +28,6 @@ if (anni > 65) {
   prezzoBiglietto = prezzoBiglietto - sconto40;
 }
 
-console.log(`"Prezzo del biglietto: € ${prezzoBiglietto}`);
+console.log(`"Prezzo del biglietto: € ${prezzoBiglietto.toFixed(2)}`);
 
-alert(`Il tuo biglietto costa € ${prezzoBiglietto}`);
+alert(`Il tuo biglietto costa € ${prezzoBiglietto.toFixed(2)}`);
